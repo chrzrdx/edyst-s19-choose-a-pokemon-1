4ba427d28825a2694d2f10dee7039eedcf590def
haripriya12 <haripriya12>
2019-04-21 19:13:47 +0530

    commit message

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,22 @@
 // import the express module
-var express = require("express");   
+const express = require('express');   
 
 // create a new express instance that you can assign to a variable
-var app = express();                       
+const app = express();  
+
+const pokemon_data = {
+    "pokemon" : ["bulbasaur", "charmander", "squirtle"]
+}
 
 // lets create a get function that returns a list of users
-app.get("/api/pokemon", (req, res, next) => {
-    res.json(["bulbasaur","charmander", "squirtle"  ]);
+app.get('/api/pokemon', (req, res) => {
+    res.json(pokemon_data);
    });
+
+   //if we enter invalid url
+   app.get('/*', (req,res)=>{
+    res.status(404).send({erroe : "please, enter the valid url"});
+  })
 
    // set our app to listening to port 8006 and creates a callback function..
 app.listen(8006, () => {
